11595574decbc969e65c91a6db8002026ff80c5f
David Ruiz <david.ruizluque@usp.ceu.es>
2023-03-30 10:13:48 +0200

    cambios finales

--- ejercicio6.py
+++ ejercicio6.py
@@ -113,15 +113,3 @@ class Matrix(object):
         
 if __name__ == '__main__':
     m = Matrix(3, 3)
-    m[0][0] = 1
-    m[0][1] = 2
-    m[0][2] = 3
-    m[1][0] = 4
-    m[1][1] = 5
-    m[1][2] = 6
-    m[2][0] = 7
-    m[2][1] = 8
-    m[2][2] = 9
-    print(m)
-    print(m.determinante_iterativo())
-    print(m.determinante_recursivo())
